5f6df8ea08f1255a3212c229d1ccc54591ee3de1
diptisahu842006@gmail.com <dipti.2528@itmuniversity.org>
2026-09-19 13:05:51 +0530

    Updated while and do while loop programs

--- src/dowhileloop.java
+++ src/dowhileloop.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class dowhileloop {
+    public static void main(String args[] ){
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=20);
+    }
 }
--- src/whileloop.java
+++ src/whileloop.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class whileloop {
-}
+        public static void main(String args[]){
+           int i=0;
+           while(i<=20){
+               System.out.println(i);
+               i++;
+            }
+        }
+    }
+
+
